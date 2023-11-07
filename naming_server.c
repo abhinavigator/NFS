02a8ce5709f1f4b324cfc0a802e5a8fa7b4d4c11
Abhinav anand <77060982+abhinavigator@users.noreply.github.com>
2023-11-07 13:45:38 +0530

    corrected the code in naming_server

--- naming_server.c
+++ naming_server.c
@@ -78,8 +78,34 @@ int main() {
     int opt = 1;
     int addrlen = sizeof(address);
     
-    // [Initialize server socket, bind, and listen...]
-    
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 12345
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind the socket to the address and port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 10) < 0) { // 10 is the maximum size of the pending connections queue
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
     printf("Naming Server is running on port %d\n", PORT);
     
     while (1) {
@@ -98,7 +124,8 @@ int main() {
         close(new_socket);
     }
     
-    // [Cleanup and close server socket before exiting]
+    // Close the server socket before exiting
+    close(server_fd);
     
     return 0;
 }
