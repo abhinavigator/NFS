6f93b2fcca2a22530d3926a2e2d4d6f03ae6e4ba
Tanay Gad <tanay.gad@students.iiit.ac.in>
2023-11-14 17:56:39 +0530

    Changes to commstruct

--- commstruct.h
+++ commstruct.h
@@ -4,10 +4,13 @@
 
 #define MAX_ARGS 100
 #define MAX_ARGS_LENGTH 1024
+#define MAX_PATH_SIZE 1024
 
 typedef struct commstruct {
     int type;                   // 0 for NM, 1 for SS, 2 for client
     char data[MAX_ARGS][MAX_ARGS_LENGTH];
     char path[MAX_PATH_SIZE];
+    char ip[INET_ADDRSTRLEN];
+    int port;
     int num_args;
 }commstruct;
